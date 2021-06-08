7bbe073a8b7e9629a2949ef48b0cd21559bab8ac
grawlixes <kfranke1@binghamton.edu>
2021-06-07 23:34:08 -0400

    basic third person controller done

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -14,34 +14,84 @@ public class ThirdPersonMovement : MonoBehaviour
     // Why is this necessary, then? Does the value usually matter?
     float turnVelocity;
 
-    // Update is called once per frame
-    void Update()
+    // Given a vector (x, y), this function returns the full angle (not reference angle) to that vector in radians.
+    float ProperArctan(float x, float y) 
     {
-        /* Tasks:
-         * 1. Make the camera look in the direction the player is facing.
-         * 2. Make the player turn with the camera when moving with the mouse. DONE
-         */
+        float opp;
+        float adj;
 
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        float ret = 0;
+        if (y > 0 || (y == 0 && x < 0)) 
+        {
+            opp = Mathf.Abs(y);
+            adj = Mathf.Abs(x);
 
+            if (x < 0)
+                ret = 3 * Mathf.PI / 2; 
+        } else
+        {
+            opp = Mathf.Abs(x);
+            adj = Mathf.Abs(y);
+
+            if (x > 0)
+                ret = Mathf.PI / 2;
+            else
+                ret = Mathf.PI;
+        }
+
+        //Debug.Log("offset " + (ret * Mathf.Rad2Deg).ToString());
+
+        if (adj != 0)
+            ret += Mathf.Atan2(opp, adj);
+
+        return ret;
+    }
+
+    private void Start()
+    {
+        Debug.Log(ProperArctan(0, 1));
+        Debug.Log(ProperArctan(1, 1));
+        Debug.Log(ProperArctan(1, 0));
+        Debug.Log(ProperArctan(1, -1));
+        Debug.Log(ProperArctan(0, -1));
+        Debug.Log(ProperArctan(-1, -1));
+        Debug.Log(ProperArctan(-1, 0));
+        Debug.Log(ProperArctan(-1, 1));
+    }
+
+    private void MakePlayerFaceCameraDirection() 
+    {
         float cameraAngle = cameraTransform.eulerAngles.y;
 
-        // Ensure that the direction is relative to the camera's current direction.
-        /* Todo: fix this, it doesn't work right now. How can we ensure that the player is always moving relative to the camera?
-         * We can probably use the player's direction to help us, because it will always reflect where the camera's pointing.
-         */
-        Vector3 direction = new Vector3(horizontal * Mathf.Sin(cameraAngle), 0f, vertical * Mathf.Cos(cameraAngle)).normalized;
-
-        // Make the player face the direction of the camera and current movement.
-        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraAngle;
         // This "SmoothDampAngle" function ensures that we smoothly look at the new direction instead of snapping to it.
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnSmoothTime);
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cameraAngle, ref turnVelocity, turnSmoothTime);
         transform.rotation = Quaternion.Euler(0f, angle, 0f);
+    }
 
-        if (direction.magnitude >= 0.1f)
+    private void TryToMove()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        float cameraAngle = cameraTransform.eulerAngles.y;
+
+        if (horizontal != 0 || vertical != 0)
         {
-            controller.Move(direction * speed * Time.deltaTime);
+            float cameraAngleRad = cameraAngle * Mathf.Deg2Rad;
+            float movementDirectionalOffsetRad = ProperArctan(horizontal, vertical);
+            float xMagnitude = Mathf.Sin(cameraAngleRad + movementDirectionalOffsetRad);
+            float yMagnitude = Mathf.Cos(cameraAngleRad + movementDirectionalOffsetRad);
+
+            // Ensure that the direction is relative to the camera's current direction.
+            // "normalized" makes the vector's length 1 while keeping the proportions of its dimensions.
+            Vector3 movement = new Vector3(xMagnitude, 0f, yMagnitude).normalized;
+            controller.Move(speed * Time.deltaTime * movement);
         }
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        MakePlayerFaceCameraDirection();
+        TryToMove();
     }
 }
